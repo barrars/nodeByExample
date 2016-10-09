fb4c2f56bc780cabe65bf7e88b9b9ebae2dfeff4
barrars <barrars@gmail.com>
2016-10-08 22:52:58 -0700

    "maybe SSH now?"

--- book.js
+++ book.js
@@ -1,4 +1,4 @@
 exports.name = 'Node.js by example with scotty McPoo';
 exports.read = function() {
-	console.log('I am reading ' + exports.name);
+	console.log('I am reading this book called' + exports.name);
 }
