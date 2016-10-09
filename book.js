ed1a78056266c1523ef7145bc3207c16ab568e7c
barrars <barrars@gmail.com>
2016-10-08 22:50:49 -0700

    "SSH????"

--- book.js
+++ book.js
@@ -1,4 +1,4 @@
-exports.name = 'Node.js by example with scotty';
+exports.name = 'Node.js by example with scotty McPoo';
 exports.read = function() {
 	console.log('I am reading ' + exports.name);
 }
